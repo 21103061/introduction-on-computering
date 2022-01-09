94ff6607d112cdf96685f024feea8a595696c755
21103061 <96883521+21103061@users.noreply.github.com>
2022-01-09 10:24:17 -0800

    Add files via upload

---
+++ python assignment-1 (21103061).py
@@ -0,0 +1,137 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+num1=int(input("Enter the value of a"))
+Enter the value of a 35
+num2=int(input("Enter the value of b"))
+Enter the value of b 55
+num3=int(input("Enter the value of c"))
+Enter the value of c 75
+x=(num1+num2+num3)/3
+print("Average of three values")
+Average of three values
+print("Average of three value : ",x)
+Average of three value :  55.0
+
+========================================================================== RESTART: Shell =========================================================================
+a=int(input("Enter the gross income(in nearest penny)" $)
+      
+SyntaxError: invalid syntax
+
+========================================================================== RESTART: Shell =========================================================================
+a=int(input("Enter the gross income(in nearest penny)")
+      $ 100000
+b=int(input("Enter number of your dependents"):
+      
+SyntaxError: invalid syntax
+
+
+a=input("Enter the gross income(in neareast penny)")
+      
+Enter the gross income(in neareast penny) $100000
+b=input("Enter number of your dependents")
+      
+Enter number of your dependents 3
+print("your tax")= 16200.0
+      
+SyntaxError: cannot assign to function call here. Maybe you meant '==' instead of '='?
+print("your tax")
+      
+your tax
+tax $16200.0
+      
+SyntaxError: invalid syntax
+print("your tax") = $ 16200
+      
+SyntaxError: cannot assign to function call
+KeyboardInterrupt
+print("your tax")
+      
+your tax
+your tax 16200
+      
+SyntaxError: invalid syntax
+
+
+========================================================================== RESTART: Shell =========================================================================
+
+
+a=input("Enter SID")
+      
+Enter SID = 21103061
+b=input("Enter Name")
+      
+Enter Name = MUSKAN
+C=input("Enter Gender")
+      
+Enter Gender = female
+d=input("Enter Branch")
+      
+Enter Branch = CSE
+e=input("Enter CGPA")
+      
+Enter CGPA = 9.4
+
+
+
+========================================================================== RESTART: Shell =========================================================================
+
+
+a=input("Enter marks of 1st student")
+      
+Enter marks of 1st student = 88
+b=input("Enter marks of 2nd student")
+      
+Enter marks of 2nd student = 93
+c=input("Enter marks of 3rd student")
+      
+Enter marks of 3rd student = 84
+d=input("Enter marks of 4th student")
+      
+Enter marks of 4th student = 75
+e=input("Enter marks of 5th student")
+      
+Enter marks of 5th student = 89
+print("Sorted list in increasing order")
+      
+Sorted list in increasing order
+print("Enter the sorted list in increasing order")
+      
+Enter the sorted list in increasing order
+[75,84,88,89,93]
+      
+[75, 84, 88, 89, 93]
+print("Enter the sorted list in decreasing order")
+      
+Enter the sorted list in decreasing order
+[93,89,88,84,75]
+      
+[93, 89, 88, 84, 75]
+
+
+
+========================================================================== RESTART: Shell =========================================================================
+
+
+(A) print['Red' 'Green' 'White' 'Pink' 'Yellow']
+      
+SyntaxError: invalid syntax
+
+========================================================================== RESTART: Shell =========================================================================
+(a) print('Red' 'Green' 'White' 'Pink' 'Yellow')
+      
+SyntaxError: invalid syntax
+
+
+
+print('red' 'green' 'white' 'pink' 'yellow')
+      
+redgreenwhitepinkyellow
+
+
+
+========================================================================== RESTART: Shell =========================================================================
+
+
+print('red , green , white , purple , yellow ')
+      
+red , green , white , purple , yellow 
