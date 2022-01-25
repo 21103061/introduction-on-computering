5c922878e4668c8a40411a51f944140c08c604b9
21103061 <96883521+21103061@users.noreply.github.com>
2022-01-25 01:43:31 -0800

    Add files via upload

---
+++ Output_ass-2.py
@@ -0,0 +1,86 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+input_string = input("Enter the string:(Leave blank for default)")
+Enter the string:(Leave blank for default)
+a=input("Enter the length of the input_string")
+Enter the length of the input_string 35
+b=input("Enter the reverse of string is 'egaugnal evitisnes esac a si nohtyp'")
+Enter the reverse of string is 'egaugnal evitisnes esac a si nohtyp'
+e=input("Enter the index of substring a")
+Enter the index of substring a 10
+f=input("Enter the original string after removing whitespaces")
+Enter the original string after removing whitespaces
+
+
+================================ RESTART: Shell ================================
+
+a=input("Enter name")
+Enter name 'MUSKAN'
+b=input("Enter your SID")
+Enter your SID 21103061
+C=input("Enter ypur department ")
+Enter ypur department CSE
+d=input("Enter your CGPA")
+Enter your CGPA 9.9
+
+
+================================ RESTART: Shell ================================
+
+
+a=int("Enter the value of a")
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    a=int("Enter the value of a")
+ValueError: invalid literal for int() with base 10: 'Enter the value of a'
+
+================================ RESTART: Shell ================================
+a=input("Enter the value of a")
+Enter the value of a 8
+b=input("Enter the value of b")
+Enter the value of b 58
+c=input("Enter the value of c")
+Enter the value of c 50
+d=input("Enter the left shift both a and b with 2 bits respectively")
+Enter the left shift both a and b with 2 bits respectively 224 , 40
+e=input("Enter the right shift a with 2 bits and 4 bits respectively")
+Enter the right shift a with 2 bits and 4 bits respectively 14,0
+
+
+================================ RESTART: Shell ================================
+
+a=input("Enter first number")
+Enter first number 4
+b=input("Enter second number")
+Enter second number 5
+c=input("Enter third number")
+Enter third number 6
+input("Enter the largest number")
+Enter the largest number 6
+' 6'
+
+
+================================ RESTART: Shell ================================
+
+
+a=input("Enter the string")
+Enter the string 
+input("Enter My name")
+Enter My name is MUSKAN
+' is MUSKAN'
+
+
+================================ RESTART: Shell ================================
+
+
+a=input("Enter the first side of a triangle")
+Enter the first side of a triangle 5
+b=input("Enter the second side of a triangle")
+Enter the second side of a triangle 6
+c=input("Enter the third side of a triangle")
+Enter the third side of a triangle 7
+input("Enter the given input lengths can form a triangle or not (Yes or No)")
+Enter the given input lengths can form a triangle or not (Yes or No) YES
+' YES'
+
+
+================================ RESTART: Shell ================================
