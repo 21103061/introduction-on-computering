b6aff131effd8a3ef323d73515df7218114eae81
21103061 <96883521+21103061@users.noreply.github.com>
2022-02-14 06:29:20 -0800

    Add files via upload

---
+++ Output ass-3.py
@@ -0,0 +1,75 @@
+Question1:
+Enter any string: Muskan
+{'M': 1, 'S': 1, 'H' : 1, 'A' :1, 'N':1 , 'K':1}
+
+Question2:
+    Enter the day : 14
+    Enter the month of the year : 01
+    Enter the year : 2002
+    15 / 01 / 2002
+
+Question3:
+    Enter elements of a list separated by space 3 , 9 , 10
+    list: 3 , 9 , 10
+    [(3,9),(9,81),(10,100)]
+
+Question4:
+    Enter grade point: 5
+    Your grade is 'C' and below average performance
+
+
+Question5:
+    ABCDEFGHIJK
+    ABCDEFGHI
+    ABCDEFG
+    ABCDE
+    ABC
+    A
+
+
+Question6:
+    Enter SID: 21103061
+    Enter Name: Muskan
+    Do you want to enter another student entry(Y OR N):Y
+    Enter SID: 211030110
+    Enter Name: Shrey
+    Do you want to enter another student entry(Y OR N):Y
+    Enter SID: 21104091
+    Enter Name: Tarun
+    Do you want to enter another student entry(Y OR N):Y
+    Enter SID: 21105098
+    Enter Name: Harshi
+    Do you want to enter another student entry(Y OR N):Y
+    <a> {21103061: 'Muskan' , 211030110: 'Shrey' , 21104091: 'Tarun' , 21105098: 'Harshi'}
+    <b> {21105098: 'Harshi' , 21103061:  'Muskan' , 211030110: 'Shrey' , 21104091: 'Tarun'}
+    <c> {21103061: 'Muskan' , 211030110: 'Shrey' , 21104091: 'Tarun' , 21105098: 'Harshi'}
+    Search Name with SID: 211030110
+    <d> Shrey
+
+
+Question7:
+    Enter the number of terms in the series: 10
+    Fibonacci sequence:
+        0
+        1
+        1
+        2
+        3
+        5
+        8
+        13
+        21
+        34
+        Average of resultant fibonacci series is 8.9
+
+
+Question8:
+    <a> {1,3,5,6,8}
+    <b>  {3,6,8,9,13,17}
+    <c> {1,2,4,5}
+    <d> {6,7,8,9,10}
+    <e> {10,7}
+    
+    
+    
+
