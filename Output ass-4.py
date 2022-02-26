b9617c3a2b969182c870adb6b0cbd147aa8e8c13
21103061 <96883521+21103061@users.noreply.github.com>
2022-02-26 12:18:47 -0800

    Add files via upload

---
+++ Output ass-4.py
@@ -0,0 +1,64 @@
+
+
+ANSWER1:
+    Move disk 1 from rod A to rod C
+    Move disk 2 from rod A to rod B
+    Move disk 1 from rod C to rod B
+    Move disk 3 from rod A to rod C
+    Move disk 1  from rod B  to rod A
+    Move disk 2 from rod  B to rod C
+    Move disk 1  from rod A  to rod C
+
+
+
+ANSWER2:
+    Enter the size of pascals triangle 5
+    using loops
+       1
+      1 1
+     1 2 1
+    1 3 3 1
+   1 4 6 4 1
+
+   using recurssion
+      1
+    1  1
+   1  2  1
+  1  3  3  1    
+ 1  4  6  4  1
+
+
+ ANSWER3:
+     Enter two numbers: 10 , 3
+     (a) Checking if the quotient and remainder is a callable value or not
+     False
+     False
+     <b> All the values are non zero
+     <c> Filtered out numbers that are greater than 4 are:[7,5,6,8,9,7]
+     <d> Set:{5,6,7,8,9}
+     <e> Immutable set: frozenset({5,6,7,8,9})
+     <f> Hash value of the max value from the above set: 9
+
+
+ ANSWER4:
+     Object created
+     The name of the student it Muskan and SID is 21103061
+     Object destroyed
+
+
+
+ANSWER5:
+    <a> The updated salary of Muskan is 70000
+    <b> Record of Komal deleted
+    
+
+
+ANSWER6:
+    Enter the word: deal
+    Enter a new meaningful word using the exact same letters to test your friendship: lead
+    Does the word makes sense?(y/Y or n/N )y
+    The friend pass the friendship test
+     
+     
+     
+     
